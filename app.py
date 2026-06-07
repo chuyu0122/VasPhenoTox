b3fe8e1975871d81b472027d7244d7a236da1d3b
chuyu0122 <chuyu0122@gmail.com>
2026-06-07 20:01:16 +0800

    Update app.py

--- app.py
+++ app.py
@@ -286,14 +286,11 @@ if st.session_state.page == "Introduction":
                     <div class="centered-content">
                         <img src="data:image/jpeg;base64,{encoded_string}" width="800" style="display: block; margin: 0 auto;">
                         <p style="font-size: 22px; margin-top: 10px; text-align: center;">
-                            <strong>Development of an Automated Morphometric Approach to Assess Vascular Outcomes</strong><br>
-                            <strong>following Exposure to Environmental Chemicals in Zebrafish</strong><br>
-                            <a href="https://doi.org/10.1289/EHP13214
+                
         
-        
-        
-        "Citation：Zhong X, Chen J, Zhang Z, Zhu Q, Ji D, Ke W, Niu C, Wang C, Zhao N, Chen W, Jia K, Liu Q, Song M, Liu C, Wei Y.Development of an Automated Morphometric Approach to 
-        Assess Vascular Outcomes following Exposure to Environmental Chemicals in Zebrafish.Environ Health Perspect.doi: 10.1289/EHP13214.</p>
+        <strong>Citation：
+        Zhong X, Chen J, Zhang Z, Zhu Q, Ji D, Ke W, Niu C, Wang C, Zhao N, Chen W, Jia K, Liu Q, Song M, Liu C, Wei Y.Development of an Automated Morphometric Approach to 
+        <strong> Assess Vascular Outcomes following Exposure to Environmental Chemicals in Zebrafish.Environ Health Perspect.doi: 10.1289/EHP13214.</p>
                     </div>
                     """,
             unsafe_allow_html=True
